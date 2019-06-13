3dd0127af702afd140d08a1bbbcc489eb19cf57f
larsfoll <lars.follet@gmail.com>
2019-06-13 15:49:19 +0200

    Add readme

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql')
 
-// Database connection
-const db = mysql.createConnection({
+const dbConfig = {
   host: process.env.DOMAIN,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
-})
+}
+
+// Database connection
+const db = mysql.createConnection(dbConfig)
 
 db.connect()
 
